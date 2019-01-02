b4dfcd65abb2715cdd4be75b4030107af22a8fcf
Kyle <35588499+SDBagel@users.noreply.github.com>
2019-01-01 17:10:43 -0800

    Add project files.

---
+++ MultispatialLogistics/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MultispatialLogistics.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
